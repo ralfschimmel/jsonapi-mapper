54326bce83bf9ddf601514270626a967fcb21ff1
Ralf Schimmel <r.schimmel@gynzy.com>
2016-06-13 23:18:42 +0200

    Add a function to recurse through relations when building the template.

--- src/bookshelf/mapper.ts
+++ src/bookshelf/mapper.ts
@@ -28,6 +28,43 @@ export default class Bookshelf implements I.Mapper {
   }
 
   /**
+   * Recursively add all the (nested) relations to the template
+   * @param data
+   * @param type
+   * @param bookshelfOptions
+   * @param template
+   */
+  mapRelations(model: any, type: string, bookshelfOptions: I.BookshelfOptions = {relations: true}, template?: Serializer.ISerializerOptions): any {
+    let self: this = this;
+    _.forOwn(model.relations, function (relModel: Model, relName: string): void {
+
+        // Skip if the relation is not permitted
+        if (bookshelfOptions.relations === false ||
+          (typeCheck('[String]', bookshelfOptions.relations) &&
+          (<string[]> bookshelfOptions.relations).indexOf(relName) < 0)) {
+
+          return;
+        }
+
+        // Avoid duplicates
+        if (!_.include(template.attributes, relName)) {
+          // Add relation to attribute list
+          template.attributes.push(relName);
+        }
+
+        // Apply relation attributes
+        if (template[relName] === undefined || _.isEmpty(template[relName].attributes)) {
+
+          // Add relation serialization
+          template[relName] = utils.buildRelation(self.baseUrl, type, relName, utils.getDataAttributesList(relModel), true);
+        }
+
+        // recurse to add nested relations
+        self.mapRelations(relModel, type, bookshelfOptions, template[relName]);
+    });
+  }
+
+  /**
    * Maps bookshelf data to a JSON-API 1.0 compliant object
    * @param data
    * @param type
@@ -44,42 +81,17 @@ export default class Bookshelf implements I.Mapper {
     template.topLevelLinks = links.buildTop(self.baseUrl, type, bookshelfOptions.pagination, bookshelfOptions.query);
     template.dataLinks = links.buildSelf(self.baseUrl, type, null, bookshelfOptions.query);
 
+    // Provide support for withRelated option TODO WARNING DEPRECATED. To be deleted on next major version
+    if (bookshelfOptions.includeRelations) bookshelfOptions.relations = bookshelfOptions.includeRelations;
+
     // Serializer process for a Model
     if (isModel(data)) {
       // Add list of valid attributes
       template.attributes = utils.getDataAttributesList(data);
 
-      // Provide support for withRelated option TODO WARNING DEPRECATED. To be deleted on next major version
-      if (bookshelfOptions.includeRelations) bookshelfOptions.relations = bookshelfOptions.includeRelations;
-
       // Add relations (only if permitted)
       if (bookshelfOptions.relations) {
-        _.forOwn(data.relations, function (relModel: Model, relName: string): void {
-
-          // Skip if the relation is not permitted
-          if (bookshelfOptions.relations === false ||
-            (typeCheck('[String]', bookshelfOptions.relations) &&
-            (<string[]> bookshelfOptions.relations).indexOf(relName) < 0)) {
-
-            return;
-          }
-
-          // Add relation to attribute list
-          template.attributes.push(relName);
-
-          // Add relation serialization
-          template[relName] = utils.buildRelation(self.baseUrl, type, relName, utils.getDataAttributesList(relModel), true);
-
-          // Support a nested relationship
-          _.forOwn(relModel.relations, function (nestedRelModel: Model, nestedRelName: string): void {
-
-              // Add relation to attribute list
-              template[relName].attributes.push(nestedRelName);
-
-              // Add nested relation serialization
-              template[relName][nestedRelName] = utils.buildRelation(self.baseUrl, relName, nestedRelName, utils.getDataAttributesList(nestedRelModel), true);
-          });
-        });
+        self.mapRelations(data, type, bookshelfOptions, template);
       }
 
       // Serializer process for a Collection
@@ -92,52 +104,8 @@ export default class Bookshelf implements I.Mapper {
         // Add list of valid attributes
         template.attributes = utils.getDataAttributesList(model);
 
-        // Provide support for withRelated option TODO WARNING DEPRECATED. To be deleted on next major version
-        if (bookshelfOptions.includeRelations) bookshelfOptions.relations = bookshelfOptions.includeRelations;
-
         data.forEach((model) => {
-          _.forOwn(model.relations, function (relModel: Model, relName: string): void {
-
-              // Skip if the relation is not permitted
-              if (bookshelfOptions.relations === false ||
-                (typeCheck('[String]', bookshelfOptions.relations) &&
-                (<string[]> bookshelfOptions.relations).indexOf(relName) < 0)) {
-
-                return;
-              }
-
-              // Avoid duplicates
-              if (!_.include(template.attributes, relName)) {
-                // Add relation to attribute list
-                template.attributes.push(relName);
-              }
-
-              // Apply relation attributes
-              if (template[relName] === undefined || _.isEmpty(template[relName].attributes)) {
-
-                // Add relation serialization
-                template[relName] = utils.buildRelation(self.baseUrl, type, relName, utils.getDataAttributesList(relModel), true);
-              }
-
-              // Support a nested relationship
-              _.forOwn(relModel.relations, function (nestedRelModel: Model, nestedRelName: string): void {
-
-                  // Avoid duplicates
-                  if (!_.includes(template[relName].attributes, nestedRelName)) {
-
-                      // Add relation to attribute list
-                      template[relName].attributes.push(nestedRelName);
-                  }
-
-                  // Apply relation attributes
-                  if (template[relName][nestedRelName] === undefined || _.isEmpty(template[relName][nestedRelName].attributes)) {
-
-                      // Add nested relation serialization
-                      template[relName][nestedRelName] = utils.buildRelation(self.baseUrl, relName, nestedRelName, utils.getDataAttributesList(nestedRelModel), true);
-                  }
-              });
-
-          });
+          self.mapRelations(model, type, bookshelfOptions, template);
         });
 
       }
